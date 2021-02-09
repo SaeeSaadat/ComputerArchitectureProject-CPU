678d36c64342d030f1fcc61ddc434bf00fd7fb57
Mahdi Asmae <m.asmae@student.sharif.edu>
2021-02-08 22:25:54 -0800

    alu added to datapath

---
+++ src/ZERO_EXTENDER/ZERO_EXTENDER.v
@@ -0,0 +1,12 @@
+module zero_extender #(
+    parameter in_width = 1,
+    parameter out_width = 32
+) (
+    input    [in_width - 1 : 0]  in,
+    output   [out_width - 1 : 0] out
+);
+
+    localparam width_dif = out_width - in_width;
+    assign out = {{width_dif {0}}, in[in_width - 1 : 0]};
+
+endmodule
